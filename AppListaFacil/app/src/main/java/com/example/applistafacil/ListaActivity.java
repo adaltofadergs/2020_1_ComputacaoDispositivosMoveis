405068072c6c813aba695b9137719ee3267b451d
Adalto Selau Sparremberger <adaltoss@MacBook-Pro-de-Adalto.local>
2020-05-08 20:39:17 -0300

    Construído o Formulário para salvar no Firebase

--- AppListaFacil/app/src/main/java/com/example/applistafacil/ListaActivity.java
+++ AppListaFacil/app/src/main/java/com/example/applistafacil/ListaActivity.java
@@ -1,5 +1,6 @@
 package com.example.applistafacil;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -23,8 +24,8 @@ public class ListaActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(ListaActivity.this, FormularioActivity.class);
+                startActivity( intent );
             }
         });
     }
